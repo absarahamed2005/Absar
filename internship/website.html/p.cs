9f59697ae4d3ef6944aa217216c90f55483c1fc8
absarahamed2005 <absarahamed2005@gmail.com>
2024-09-25 09:56:40 +0530

    Add files via upload

---
+++ internship/website.html/p.cs
@@ -0,0 +1,126 @@
+*{
+    margin:0px;
+    padding:0px;
+}
+
+section{
+    margin-top:150px;
+    margin-left:50px;
+   
+}
+
+.box{
+    background-color:#515151;
+    height:400px;
+    width:350px; 
+    display:inline-block;
+    background-image:url("./logo512\ \(11\).png") ; 
+    background-size:230px;
+    background-repeat:no-repeat;
+    background-position:center;
+    border-radius:15px;
+
+    
+    
+}
+
+.box p{
+    text-align:center;
+    justify-content: center;
+    display:inline-block;
+    color:white;
+    padding:10px;
+    display:flex;
+    
+}
+.box h4{
+text-align:center;
+color:blue;
+padding:20px;   
+}
+.classroom{
+    background-color:rgb(201, 175, 175);
+    width:180px;
+    height:130px;
+    align-items:center;
+    margin-left:500px;
+    margin-bottom:900px;
+    padding-top: 100px;
+    text-align:center;
+    position:absolute;
+    top:140px;
+    
+    
+    
+    
+}
+.rboxl{
+    margin-bottom:800px;
+}
+
+
+.college{
+    padding-top:30px;
+    background-color:rgb(201, 175, 175);
+    width:180px;
+    height:130px;
+    justify-content: end;
+    align-items:center;
+    margin-left:500px;
+    margin-bottom:950px;
+    padding-top: 100px;
+    margin-bottom:300px;
+    text-align:center;
+    position:absolute;
+    top: 2px;
+    margin-left:220px;
+
+}
+
+.certifications{
+    padding-top:30px;
+    background-color:rgb(201, 175, 175);
+    width:180px;
+    height:130px;
+    justify-content: end;
+    align-items:center;
+    margin-left:500px;
+    margin-bottom:900px;
+    padding-top: 100px;
+    margin-bottom:300px;
+    text-align:center;
+    margin-top:100px;
+    margin:30px 0px 0px 0px;
+
+
+}
+
+
+.place{
+    padding-top:30px;
+    background-color:rgb(201, 175, 175);
+    width:180px;
+    height:130px;
+    justify-content: end;
+    align-items:center;
+    margin-left:500px;
+    margin-bottom:900px;
+    padding-top: 100px;
+    margin-bottom:300px;
+    text-align:center;
+    margin-top:100px;
+    margin:30px 0px 0px 0px;
+    margin: left 19px;
+    
+    
+
+    
+
+
+}
+section{
+    display:flex;
+    margin-left:100px;
+    
+
+}
